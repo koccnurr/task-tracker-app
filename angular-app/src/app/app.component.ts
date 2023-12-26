738f50f0b4da2c67fd63b721716200c8ff2b46f0
Nur Koç <nurkoc7979@gmail.com>
2023-12-26 19:40:28 +0300

    fix control

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterModule } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { ProjectComponent } from './component/project/project.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,RouterModule, RouterOutlet,HeaderComponent,ProjectComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angular-app';
 }
--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProjectComponent } from './component/project/project.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',  // Remove the slash at the beginning
+        component: HomeComponent
+    },
+    {
+        path: 'project',  // Remove the slash at the beginning
+        component: ProjectComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
